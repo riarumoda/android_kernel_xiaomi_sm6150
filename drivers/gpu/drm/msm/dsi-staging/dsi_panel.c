0994c015b8ff1a7a91a294966a8b8a6dbd3eb3ed
Nauval Rizky <enuma.alrizky@gmail.com>
2020-03-09 11:54:52 +0900

    drm: msm: adapt exposure adjustment methods to sweet
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -45,6 +45,10 @@
 #include <linux/double_click.h>
 #include "xiaomi_frame_stat.h"
 
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+#include "exposure_adjustment.h"
+#endif
+
 /**
  * topology is currently defined by a set of following 3 values:
  * 1. num of layer mixers
@@ -963,7 +967,7 @@ bool dc_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 {
 	int rc = 0;
-
+	int bl_dc_min = panel->bl_config.bl_min_level * 2;
 	struct dsi_backlight_config *bl = &panel->bl_config;
 
 	if (panel->host_config.ext_bridge_num)
@@ -982,6 +986,11 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 			pr_info("skip set backlight=0 bacase fod_dimlayer_hbm_enabled enable");
 			return 0;
 		}
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+	if (bl_lvl > 0)
+		bl_lvl = ea_panel_calc_backlight(bl_lvl < bl_dc_min ? bl_dc_min : bl_lvl);
+#endif
+
 		dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_DISP_DIMMINGOFF);
 	}
 
