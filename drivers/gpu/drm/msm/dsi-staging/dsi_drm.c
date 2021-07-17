882ce696ff283a8227587578a7753c5b4359728c
Nauval Rizky <enuma.alrizky@gmail.com>
2021-07-17 14:24:28 +0700

    msm: dsi-staging: Remove unnecessary wakelock for the panel
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
@@ -46,41 +46,7 @@ static struct dsi_display_mode_priv_info default_priv_info = {
 	.dsc_enabled = false,
 };
 
-static struct wakeup_source prim_panel_wakelock;
-
-struct drm_notify_data g_notify_data;
-
-/*
- *	drm_register_client - register a client notifier
- *	@nb:notifier block to callback when event happen
- */
-int drm_register_client(struct notifier_block *nb)
-{
-	return blocking_notifier_chain_register(&drm_notifier_list, nb);
-}
-EXPORT_SYMBOL(drm_register_client);
-
-/*
- *	drm_unregister_client - unregister a client notifier
- *	@nb:notifier block to callback when event happen
- */
-int drm_unregister_client(struct notifier_block *nb)
-{
-	return blocking_notifier_chain_unregister(&drm_notifier_list, nb);
-}
-EXPORT_SYMBOL(drm_unregister_client);
-
-/*
- *	drm_notifier_call_chain - notify clients of drm_event
- *
- */
-
-int drm_notifier_call_chain(unsigned long val, void *v)
-{
-	return blocking_notifier_call_chain(&drm_notifier_list, val, v);
-}
-EXPORT_SYMBOL(drm_notifier_call_chain);
-
+struct msm_drm_notifier g_notify_data;
 
 static void convert_to_dsi_mode(const struct drm_display_mode *drm_mode,
 				struct dsi_display_mode *dsi_mode)
@@ -251,7 +217,6 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 
 	if (atomic_read(&c_bridge->display_active)) {
 		cancel_delayed_work_sync(&c_bridge->pd_work);
-		__pm_relax(&prim_panel_wakelock);
 		if (c_bridge->display->panel->panel_mode == DSI_OP_VIDEO_MODE) {
 			pr_debug("skip set display config for video panel in fpc\n");
 			return;
@@ -535,10 +500,8 @@ static void dsi_bridge_post_disable_work(struct work_struct *work)
 	if (!bridge)
 		return;
 
-	if (atomic_read(&bridge->display_active)) {
+	if (atomic_read(&bridge->display_active))
 		dsi_bridge_post_disable(&bridge->base);
-		__pm_relax(&prim_panel_wakelock);
-	}
 }
 
 static void dsi_bridge_mode_set(struct drm_bridge *bridge,
@@ -1287,7 +1250,6 @@ struct dsi_bridge *dsi_drm_bridge_init(struct dsi_display *display,
 	mutex_init(&encoder->bridge->lock);
 
 	atomic_set(&resume_pending, 0);
-	wakeup_source_init(&prim_panel_wakelock, "prim_panel_wakelock");
 	atomic_set(&bridge->display_active, false);
 	init_waitqueue_head(&resume_wait_q);
 	INIT_DELAYED_WORK(&bridge->pd_work, dsi_bridge_post_disable_work);
@@ -1307,7 +1269,6 @@ void dsi_drm_bridge_cleanup(struct dsi_bridge *bridge)
 	if (bridge) {
 		atomic_set(&bridge->display_active, false);
 		cancel_delayed_work_sync(&bridge->pd_work);
-		wakeup_source_trash(&prim_panel_wakelock);
 	}
 
 	kfree(bridge);
